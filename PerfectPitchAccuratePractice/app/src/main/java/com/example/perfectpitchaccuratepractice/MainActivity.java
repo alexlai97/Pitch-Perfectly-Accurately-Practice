aefd0c1f73b4c767eba685237f05206741601736
姚博伦 <yaobolun@v1040-wn-rt-c-86-243.campus-dynamic.uwaterloo.ca>
2019-06-25 23:32:38 -0400

    Resolved, add navigation

--- PerfectPitchAccuratePractice/app/src/main/java/com/example/perfectpitchaccuratepractice/MainActivity.java
+++ PerfectPitchAccuratePractice/app/src/main/java/com/example/perfectpitchaccuratepractice/MainActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import android.util.Log;
 
 import android.view.View;
@@ -78,4 +82,5 @@ public class MainActivity extends AppCompatActivity {
     public void openFilter(View view){
         startActivity(new Intent(this, FilterActivity.class));
     }
+
 }
