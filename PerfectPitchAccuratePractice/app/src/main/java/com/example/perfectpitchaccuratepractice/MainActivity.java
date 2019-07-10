4ef5f18b0273b0a28ba485939877650f5fe36357
Irvingkk <irvingyyao@gmail.com>
2019-07-10 13:31:47 -0400

    add navigation drawer

--- PerfectPitchAccuratePractice/app/src/main/java/com/example/perfectpitchaccuratepractice/MainActivity.java
+++ PerfectPitchAccuratePractice/app/src/main/java/com/example/perfectpitchaccuratepractice/MainActivity.java
@@ -7,13 +7,18 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.util.Log;
 
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,20 +40,22 @@ import android.widget.TableLayout;
 /**
  * Main activity
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MAIN";
     private static PlaySound theSound = new PlaySound();
     private TextView arrow;
 
     private ModelController modelController;
     private boolean created = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.e(TAG, "s"
                 + created);
         created = true;
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.wrapper);
 
         modelController = new ModelController(new Config(), this);
         modelController.next_question();
@@ -71,6 +78,21 @@ public class MainActivity extends AppCompatActivity {
 //        // FIXME ...
 //        arrow = findViewById(R.id.arrowTextView);
 //        handleAnimation(300);
+
+
+
+        // add listener on hamburger button to open drawer
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Button nav_button = findViewById(R.id.naviButton);
+        nav_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawer.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
 
@@ -89,4 +111,38 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, FilterActivity.class));
     }
 
+    // handle drawer closing
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // listener to handle selected item
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id){
+            case R.string.note_mode:
+                Intent 
+            case R.string.interval_mode:
+                break;
+            case R.string.chord_mode:
+                break;
+            case R.string.song_mode:
+                break;
+            default:
+                break;
+        }
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
+    }
 }
