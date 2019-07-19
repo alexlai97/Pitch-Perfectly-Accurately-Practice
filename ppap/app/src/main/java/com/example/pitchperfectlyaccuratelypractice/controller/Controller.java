alexlai97/Pitch-Perfectly-Accurately-Practice

commit f821c26eb16c660d91bcbfa3f779463b1664151a

updateFrequencyText should not pass expected frequency... Fix this confusing parameter back to original (notegraph gets updated expected freqency as Controller change question)

ppap/app/src/main/java/com/example/pitchperfectlyaccuratelypractice/controller/Controller.java
@@ -216,11 +216,11 @@ public class Controller implements Observer ,
       t_out = now;
       firstTimeProcessFreq = false;
       firstStart = now;
-      updateQuestionView();
     }
+    updateQuestionView(); // FIXME should not be here, it' here because need a frequency event to update question view for the first time changing mode
 
     current_frequency = freq;
-    curFragment.updateFrequencyText(Math.round(current_frequency), getExpectedFrequencies()[0]);
+    curFragment.updateFrequencyText(Math.round(current_frequency));
     curFragment.updateCurrentPitchText("U: " + (new Note(current_frequency)).getText());
 
     double expected_freq = getExpectedFrequencies()[0];
@@ -318,10 +318,8 @@ public class Controller implements Observer ,
           break;
         case "currentFragment":
           curFragment = (GeneralFragment) event.getNewValue();
-//          Log.d(TAG, "propertyChange: Fragment" + curFragment.getClass());
-          if (curFragment.getClass() == NoteGraphFragment.class) {
-            Log.d(TAG, "propertyChange: " + "in NoteGraph Fragment");
-            model.addChangeListener((NoteGraphFragment)curFragment);
+          if (curMode == Mode.NoteGraphPractice) {
+            ((NoteGraphFragment) curFragment).setCurrentExpectedFrequency(curQuestion.getAnswerNotes()[0].getFrequency());
           }
           break;
       }

ppap/app/src/main/java/com/example/pitchperfectlyaccuratelypractice/fragments/NoteGraphFragment.java
@@ -25,7 +25,7 @@ import android.graphics.Color;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-public class NoteGraphFragment extends GeneralFragment implements PropertyChangeListener {
+public class NoteGraphFragment extends GeneralFragment {
     private static String TAG = "NoteGraphFragment";
     private Runnable mTimer;
     private Long lastFreq;
@@ -114,14 +114,12 @@ public class NoteGraphFragment extends GeneralFragment implements PropertyChange
 
 
     @Override
-    public void updateFrequencyText(Long freq, Double expectedFreq){
+    public void updateFrequencyText(Long freq){
         if(!onCreated) return;
         String temp = Long.toString(freq);
         frequencyText.setText(temp + "Hz");
-        graph.getViewport().setMaxY(expectedFreq*2);
-//        graph.getViewport().setMaxY(questionFreq*2);
+        graph.getViewport().setMaxY(questionFreq*2);
         lastFreq = freq;
-        questionFreq = expectedFreq;
     }
 
     @Override
@@ -135,16 +133,7 @@ public class NoteGraphFragment extends GeneralFragment implements PropertyChange
         if(!onCreated) return;
     }
 
-    @Override
-    public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
-        Log.d(TAG, "propertyChange: I am here!");
-        NoteQuestion noteQuestion;
-        if (propertyChangeEvent.getPropertyName() == "currentQuestion") {
-            Log.d(TAG, "propertyChange: I am here!");
-            noteQuestion = (NoteQuestion) propertyChangeEvent.getNewValue();
-            questionFreq = noteQuestion.getAnswerNotes()[0].getFrequency();
-        } else {
-            throw new AssertionError("NoteGraphFragment: Why are you calling me");
-        }
+    public void setCurrentExpectedFrequency(double freq) {
+        questionFreq = freq;
     }
 }

ppap/app/src/main/java/com/example/pitchperfectlyaccuratelypractice/fragments/GeneralFragment.java
@@ -296,9 +296,8 @@ public class GeneralFragment extends Fragment {
     /**
      * update frequency text view
      * @param freq
-     * @param expected
      */
-    public void updateFrequencyText(Long freq, Double expected){
+    public void updateFrequencyText(Long freq){
         if(!onCreated) return;
         frequencyText.setText(Long.toString(freq) + " Hz");
 //        Log.d("", "updateFrequencyText: " + freq);

commit e3e5640709fb94cff65b90d754af77d3155d601e

Fix a bug introduced in NoteGraphPractice in commit f821c26eb16c660d91bcbfa3f779463b1664151a by simply updating the expected frequency when next_question.

ppap/app/src/main/java/com/example/pitchperfectlyaccuratelypractice/controller/Controller.java
@@ -134,6 +134,9 @@ public class Controller implements Observer ,
     updateQuestionView();
     correct_mask = new boolean[curQuestion.getAnswerNotes().length];
     Log.d(TAG, "next_question: current length " + curQuestion.getAnswerNotes().length);
+    if (curMode == Mode.NoteGraphPractice) {
+      ((NoteGraphFragment) curFragment).setCurrentExpectedFrequency(curQuestion.getAnswerNotes()[0].getFrequency());
+    }
   }
 
   /**

ppap/app/src/main/java/com/example/pitchperfectlyaccuratelypractice/fragments/NoteGraphFragment.java
@@ -14,16 +14,11 @@ import android.util.Log;
 
 import com.example.pitchperfectlyaccuratelypractice.R;
 
-import com.example.pitchperfectlyaccuratelypractice.question.NoteQuestion;
-import com.example.pitchperfectlyaccuratelypractice.question.Question;
 import com.jjoe64.graphview.*;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import android.graphics.Color;
-
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 
 public class NoteGraphFragment extends GeneralFragment {
     private static String TAG = "NoteGraphFragment";
@@ -114,12 +109,12 @@ public class NoteGraphFragment extends GeneralFragment {
 
 
     @Override
-    public void updateFrequencyText(Long freq){
+    public void updateFrequencyText(Long currentFrequency){
         if(!onCreated) return;
-        String temp = Long.toString(freq);
-        frequencyText.setText(temp + "Hz");
+        String temp = Long.toString(currentFrequency);
+        frequencyText.setText(temp + " Hz");
         graph.getViewport().setMaxY(questionFreq*2);
-        lastFreq = freq;
+        lastFreq = currentFrequency;
     }
 
 
@@ -153,10 +148,10 @@ public class NoteGraphFragment extends GeneralFragment {
 
     /**
      * only for this general fragment
-     * @param freq
+     * @param expected_freq
      */
-    public void setCurrentExpectedFrequency(double freq) {
-        questionFreq = freq;
+    public void setCurrentExpectedFrequency(double expected_freq) {
+        questionFreq = expected_freq;
     }
 
 